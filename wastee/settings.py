ed98520dd153640e640226967cbd310617c2aede
Luara Lima <luaralima205@gmail.com>
2024-10-01 22:38:41 -0300

    fix: settings

--- wastee/settings.py
+++ wastee/settings.py
@@ -90,8 +90,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kikieedicoes@gmail.com'  
-EMAIL_HOST_PASSWORD = '242701lego' 
+EMAIL_HOST_USER = 'example@gmail.com'  
+EMAIL_HOST_PASSWORD = '123445' 
 
 
 WSGI_APPLICATION = 'wastee.wsgi.application'
