fcb2a251b9295b3948eb3e7e46d365767999abf0
Luara Lima <luaralima205@gmail.com>
2024-10-03 22:40:05 -0300

    feat: addding email to send confirmation

--- wastee/settings.py
+++ wastee/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 import os
 import sys
@@ -113,8 +116,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'example@gmail.com'  
-EMAIL_HOST_PASSWORD = '123445' 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 
 WSGI_APPLICATION = 'wastee.wsgi.application'
