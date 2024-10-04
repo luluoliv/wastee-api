2fdf59c1d059832072189ced4051fe1f2e649406
Luara Lima <luaralima205@gmail.com>
2024-10-03 23:41:47 -0300

    fix: sending email

--- api/utils.py
+++ api/utils.py
@@ -1,4 +1,6 @@
-from django.core.mail import send_mail, BadHeaderError
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from django.conf import settings
 import random
 from .models import ConfirmationCode
@@ -9,17 +11,18 @@ def enviar_codigo_email(email_destinatario, codigo):
         assunto = 'Seu código de confirmação'
         mensagem = f'Seu código de confirmação é: {codigo}'
         email_remetente = settings.EMAIL_HOST_USER
+        senha_remetente = settings.EMAIL_HOST_PASSWORD
+        msg = MIMEMultipart()
+        msg['From'] = email_remetente
+        msg['To'] = email_destinatario
+        msg['Subject'] = assunto
+        msg.attach(MIMEText(mensagem, 'plain'))
 
-        send_mail(
-            assunto,
-            mensagem,
-            email_remetente,
-            [email_destinatario],
-            fail_silently=False,
-        )
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
+            server.starttls()  # Inicia a conexão TLS
+            server.login(email_remetente, senha_remetente)
+            server.send_message(msg)  # Envia a mensagem
 
-    except BadHeaderError:
-        print("Cabeçalho inválido.")
     except Exception as e:
         print(f"Ocorreu um erro: {e}")
 
@@ -29,6 +32,6 @@ def gerar_codigo_confirmacao(user):
         user=user,
         confirmation_code=codigo,
         is_used=False,
-        expiration_time=timezone.now() + timezone.timedelta(minutes=10) 
+        expiration_time=timezone.now() + timezone.timedelta(minutes=10)
     )
     return codigo
